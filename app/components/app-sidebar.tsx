9a7d5a8da4d8a1ccb92946962f92203a759eb532
Federicopvs <federicopvs@gmail.com>
2025-08-28 12:04:01 +0200

    Dashboard: update nav copy

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -127,7 +127,7 @@ const data = {
 	],
 	navSettings: [
 		{
-			title: 'Settings',
+			title: 'Impostazioni',
 			url: '/dashboard/settings',
 			icon: IconSettings,
 			// items: [
@@ -145,11 +145,11 @@ const data = {
 			// 	},
 			// ],
 		},
-		{
-			title: 'Get Help',
-			url: '#',
-			icon: IconHelp,
-		},
+		// {
+		// 	title: 'Contattaci',
+		// 	url: '/#contact',
+		// 	icon: IconHelp,
+		// },
 	],
 }
 
@@ -164,7 +164,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 							className="data-[slot=sidebar-menu-button]:!p-1.5"
 						>
 							<a href="/">
-								<IconInnerShadowTop className="!size-5" />
 								<span className="text-base font-semibold">Vitae</span>
 							</a>
 						</SidebarMenuButton>
--- app/components/nav-user.tsx
+++ app/components/nav-user.tsx
@@ -92,10 +92,10 @@ export function NavUser() {
 							<DropdownMenuItem asChild>
 								<Link to="/dashboard/settings/profile">
 									<IconUserCircle />
-									Profile
+									Profilo
 								</Link>
 							</DropdownMenuItem>
-							<DropdownMenuItem asChild>
+							{/* <DropdownMenuItem asChild>
 								<Link to="/dashboard/settings/#">
 									<IconCreditCard />
 									Billing
@@ -106,7 +106,7 @@ export function NavUser() {
 									<IconNotification />
 									Notifications
 								</Link>
-							</DropdownMenuItem>
+							</DropdownMenuItem> */}
 						</DropdownMenuGroup>
 						<DropdownMenuSeparator />
 						<Form action="/logout" method="POST" ref={formRef}>
