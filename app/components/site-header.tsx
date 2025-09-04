9e53bf12d5ee4a036f815db12d9ef786298c9f49
Federicopvs <federicopvs@gmail.com>
2025-09-04 16:04:27 +0200

    Dashboard: include logout button in nav main and header

--- app/components/site-header.tsx
+++ app/components/site-header.tsx
@@ -1,9 +1,13 @@
+import { useRef } from 'react'
+import { Form, Link } from 'react-router'
 import { DynamicBreadcrumb } from '#app/components/dynamic-breadcrumb'
 import { Button } from '#app/components/ui/button'
 import { Separator } from '#app/components/ui/separator'
 import { SidebarTrigger } from '#app/components/ui/sidebar'
+import { Icon } from './ui/icon'
 
 export function SiteHeader() {
+	const formRef = useRef<HTMLFormElement>(null)
 	return (
 		<header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
 			<div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -14,16 +18,19 @@ export function SiteHeader() {
 				/>
 				<DynamicBreadcrumb />
 				<div className="ml-auto flex items-center gap-2">
-					<Button variant="ghost" asChild size="sm" className="hidden sm:flex">
-						<a
-							href="https://we.co.it"
-							rel="noopener noreferrer"
-							target="_blank"
-							className="dark:text-foreground"
-						>
-							Weco ♥︎
-						</a>
-					</Button>
+					<Link to="https://we.co.it">
+						<Button variant="ghost" className="w-full">
+							<Icon name="heart" />
+							Weco
+						</Button>
+					</Link>
+
+					<Form action="/logout" method="POST" ref={formRef}>
+						<Button variant="ghost" type="submit" className="w-full">
+							<Icon name="log-out" />
+							Esci dalla Dashboard
+						</Button>
+					</Form>
 				</div>
 			</div>
 		</header>
